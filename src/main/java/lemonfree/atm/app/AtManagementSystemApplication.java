cf75a07fa425a26ededac301d887a2fe33610241
l2jong <gkwkdlawoh@naver.com>
2021-04-04 18:53:00 +0900

    SpringBoot attached

---
+++ src/main/java/lemonfree/atm/app/AtManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package lemonfree.atm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtManagementSystemApplication.class, args);
+	}
+
+}
